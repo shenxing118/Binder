e00624abb7c3a9b31e5246f2f1fdddcfc65b8783
shenxing <shenxing@jd.com>
2018-06-28 15:38:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shenxing/binder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.shenxing.binder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int version = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
